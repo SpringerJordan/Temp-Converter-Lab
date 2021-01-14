e0bfb88b977d489464c260fc46c1e1913654386b
SpringerJordan <jordantaylor9.21@gmail.com>
2021-01-14 16:38:41 -0500

    added more javascript

--- script.js
+++ script.js
@@ -1,29 +1,67 @@
 function control(){
     let startingScale = document.getElementById("scale").value;
-    if(startingScale === 'Fahrentheit'){
-        fahrenheitConverter();
-    } else if(startingScale === 'Celsius'){
-        celsiusConverter();
+    if(startingScale === 'Fahrenheit'){
+        fahrenheitFunction();
+    } 
+    else if(startingScale === 'Celsius'){
+        celsiusFunction();
+
+    }
+    else if (startingScale === 'Kelvin'){
+        kelvinFunction();
+    }
+    else if (startingScale === 'Rankine'){
+        rankineFunction();
 
     }
 }
-function celsiusConverter(){
+
+  // this is celsius to other stuff
+function celsiusFunction(){
     let tempToConvert = parseInt(document.getElementById("textBox").value);
     let convertToFahrenheit = (tempToConvert * 9/5) + 32;
-    let convertToCelsius = (tempToConvert - 32) * 5/9;
+    let convertToCelsius = tempToConvert;
+    let convertToKelvin = tempToConvert + 273.15 ;
+    let convertToRankine = tempToConvert * 9/5 + 491.67;
     document.getElementById("fahrenheit").innerHTML = convertToFahrenheit;
     document.getElementById("temp").innerHTML = convertToCelsius;
+    document.getElementById("to-kelvin").innerHTML = convertToKelvin;
+    document.getElementById("to-rankine").innerHTML = convertToRankine;
+
 }
-function fahrenheitConverter(){
+// this it fahrenheit to other stuff
+function fahrenheitFunction(){
      let tempToConvert = parseInt(document.getElementById("textBox").value);
      let convertToCelsius = (tempToConvert - 32) * 5/9;
-     let convertToFahrenheit = (tempToConvert * 9/5) + 32;
+     let convertToFahrenheit = tempToConvert;
+     let convertToKelvin = (tempToConvert - 32) * 5/9 + 273.15;
+     let convertToRankine = tempToConvert + 459.67;
    document.getElementById("temp").innerHTML = convertToCelsius;
    document.getElementById("fahrenheit").innerHTML = convertToFahrenheit;
+   document.getElementById("to-kelvin").innerHTML = convertToKelvin;
+   document.getElementById("to-rankine").innerHTML = convertToRankine;
 }
-
-
-//function fahrenheitConverter(){
- //   let tempToConvert = parseInt(document.getElementById("textBox").value);
- //  let convertToCelsius = (tempToConvert - 32) * 5/9;
- //  document.getElementById("temp").innerHTML = convertToCelsius;
+// this is kelvin to other stuff
+function kelvinFunction(){
+    let tempToConvert = parseInt(document.getElementById("textBox").value);
+    let convertToCelsius = tempToConvert - 273.15;
+    let convertToFahrenheit = (tempToConvert - 273.15) * 9/5 + 32;
+    let convertToKelvin = tempToConvert;
+    let convertToRankine = tempToConvert * 1.8;
+  document.getElementById("temp").innerHTML = convertToCelsius;
+  document.getElementById("fahrenheit").innerHTML = convertToFahrenheit;
+  document.getElementById("to-kelvin").innerHTML = convertToKelvin;
+  document.getElementById("to-rankine").innerHTML = convertToRankine;
+}
+// this is rankine to other stuff
+function rankineFunction(){
+    let tempToConvert = parseInt(document.getElementById("textBox").value);
+    let convertToCelsius = (tempToConvert - 491.67) * 5/9;
+    let convertToFahrenheit = tempToConvert - 459.67;
+    let convertToKelvin = tempToConvert * 5/9;
+    let convertToRankine = tempToConvert;
+  document.getElementById("temp").innerHTML = convertToCelsius;
+  document.getElementById("fahrenheit").innerHTML = convertToFahrenheit;
+  document.getElementById("to-kelvin").innerHTML = convertToKelvin;
+  document.getElementById("to-rankine").innerHTML = convertToRankine;
+}
